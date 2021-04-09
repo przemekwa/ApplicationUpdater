fc7728e54cd8f9e24b524abd7f0c7e77f580cb23
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2021-04-09 14:39:46 +0200

    Fix zip problem from TC
     https://stackoverflow.com/questions/24941741/zip-entry-name-ends-in-directory-separator-character-but-contains-data

--- ApplicationUpdater/ApplicationUpdater/Processes/UnzipProcess.cs
+++ ApplicationUpdater/ApplicationUpdater/Processes/UnzipProcess.cs
@@ -1,14 +1,19 @@
-﻿using System.IO;
+﻿using SharpCompress.Archives;
+using SharpCompress.Common;
+using SharpCompress.Readers;
+using System;
+using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace ApplicationUpdater.Processes
 {
+
     public class UnzipProcess: ProcessBase, IProcess<UpdateModel>
     {
         public UnzipProcess() : base("Unzipping")
         {
         }
-
 
         public ProcesEventResult Process(UpdateModel model)
         {
@@ -17,14 +22,37 @@ namespace ApplicationUpdater.Processes
             Directory.CreateDirectory(unZipDirectory);
 
             UpdateProcess($"Unzip {model.UserParams.PathToZipFile.FullName} to {unZipDirectory}");
-
-            ZipFile.ExtractToDirectory(model.UserParams.PathToZipFile.FullName, unZipDirectory);
+            UnZip(model, unZipDirectory);
 
             model.UnZipDirectory = new DirectoryInfo(unZipDirectory);
 
             model.NewApplicationDirectory = new DirectoryInfo(Path.Combine(model.UnZipDirectory.FullName, "app"));
 
             return GetProcesEventResult(Consts.ProcesEventResult.Successful);
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/24941741/zip-entry-name-ends-in-directory-separator-character-but-contains-data
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="unZipDirectory"></param>
+        private void UnZip(UpdateModel model, string unZipDirectory)
+        {
+            var archive = ArchiveFactory.Open(model.UserParams.PathToZipFile.FullName);
+
+            var maxProgress = archive.Entries.ToList().Count;
+
+            var count = 0;
+
+            foreach (var entry in archive.Entries)
+            {
+                if (entry.IsDirectory == false)
+                {
+                    entry.WriteToDirectory(unZipDirectory, new ExtractionOptions() { ExtractFullPath = true, Overwrite = true });
+                }
+
+                UpdateProcessWithPgoressBar(++count, maxProgress, string.Empty, false);
+            }
         }
     }
 }
